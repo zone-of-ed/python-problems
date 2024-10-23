3959978764f3b736f01776ccf1e510cf16c5ded9
Surya Chandra <suryaaalthi@Suryas-MacBook-Air.local>
2024-10-23 16:15:24 +0530

    feat(added question in draft):

---
+++ test.py
@@ -0,0 +1,233 @@
+# 1. write a program that prints the sum of two numbers 2 and 3
+a = 2
+b = 3
+c = a + b
+print(c)
+
+# 2. write a program that prints the subtract of two numbers 2 and 3
+a = 2
+b = 3
+c = a - b
+print(c)
+
+
+# 3. write a program that prints the product of two numbers 2 and 3
+a = 2
+b = 3
+c = a * b
+print(c)
+
+
+# 4. write a program that prints the divide of two numbers 2 and 3
+a = 2
+b = 3
+c = a / b
+print(c)
+
+
+# 5. write a program that prints the divide of two numbers 2 and 3
+a = 2
+b = 3
+c = a / b
+print(c)
+
+# 6. write a program that prints the repeat input into 3 times
+a = "hello"
+b = a * 3
+print(b)
+
+# 7. write a program that take input and print in ---hello---
+a = "hello"
+b = "-"*3
+print(b,a,b)
+
+
+# 8. write a program that prints string 1 character
+a = "hello"
+print(a[0])
+
+
+# 9. write a program that prints string 3 character
+a = "hello"
+print(a[2])
+
+# 10. write a program that prints first and last character
+a = "hello"
+print(a[0],a[len(a)-1])
+
+# 11. write a program that prints the reverse digit and digit is 2 values
+a = "21"
+print(a[1],a[0])
+
+# 12. write a program that prints the length of the string
+a = "hello world"
+print(len(a))
+
+# 13. write a program that prints input * input length repeat
+#a = input()
+print( a * len(a))
+
+# 14. write a program that take input and last character
+#a = input()
+print( len(a) - 1)
+
+# 15. write a program that prints the input half length of the word
+#a = input()
+print( len(a) / 2)
+
+# 16. write a program that prints area of rectangle
+#a = input()
+# b = input()
+print("area of rectangle",a*b)
+
+# 17. write a program that prints perimeter of rectangle
+#a = input()
+# b = input()
+print("perimeter of rectangle",2 * (a+b))
+
+# 18. write a program to read the  percentage of girls in a class and print boy
+#a = input()
+print("percentage:",(100-a))
+
+# 19. write a program to convert kilometers to meters 1km = 1000 m
+#a = input()
+print("kilometers:",(a * 1000))
+
+# 20. write a program to prints the sum of given 2 digits number
+#a = input()
+print("sum:",int(a[0] + int(a[1])))
+
+# 21. write a program to prints the that take input as a word and index as input
+#a = input()
+#b = int(input())
+print("index:",a[b])
+
+# 22. write a program to prints the first three characters of given input
+#a = input()
+print("index:",a[:3])
+
+# 23. write a program that take string input and index input and print the work
+#a = input()
+#b = int(input())
+print("index:",a[b])
+
+# 24. write a program that take start and end indexs and print the input string
+#a = input()
+#b = int(input())
+#c = int(input())
+print("index:",a[b:c+1])
+
+# 25. write a program that take nth index and print the n character of a string
+#a = input()
+#b = int(input())
+print("index:",a[:c])
+
+# 26. write a program that take input and print last 3 characters
+#a = input()
+#b = len(a)
+print("index:",a[b-4:])
+
+# 27. write a program that take need to skip the selected index in string
+#a = input()
+#b = int(input())
+print("index:",a[:b] + a[b-1:])
+
+# 28. write a program that print area and perimeter of a square
+#a = int(input())
+
+print("area of square:",str(a * a))
+print("area of perimeter:",str(a * 4))
+
+
+
+# 29. write a program that read a number and check number is greater than 40
+#a = int(input())
+print(a > 40)
+
+
+# 30. write a program that take 2 input numbers and check is input is greater or not
+#a = int(input())
+#b = int(input())
+print(a > b)
+
+# 31. write a program that take input number and check is positive or not
+#a = int(input())
+
+print(a< 0)
+
+# 33. write a program that take 2 input numbers and check is 2 numbers are equal or not
+#a = int(input())
+#b = int(input())
+print(a == b)
+
+
+# 34. write a program that take input string and check first and last character is equal or not
+#a = input()
+#b = len(a)
+print(a[0] == a[b-1])
+
+# 35. write a program that take password and validate the length of password is greathan 8
+#a = input()
+print(len(a)>8)
+
+# 36. write a program that take 2 input string and check first 4 character are equal or not
+#a = input()
+#b = input()
+print(a[:4] == b[:4])
+
+# 37. write a program that take 2 numbers a and b check both numbeers are gerate than 10 or not
+#a = input()
+#b = input()
+print(a > 10 and b > 10)
+
+# 38. write a program that take 2 numbers a and b check both numbers are negative or not
+#a = input()
+#b = input()
+print(a < 0 and b < 0)
+
+# 39. write a program that take 2 numbers a and b check both numbers are positive or not
+#a = input()
+#b = input()
+print(a > 0 and b > 0)
+
+# 40. write a program that take number and check that number is less than 100 or not
+#a = input()
+print(a < 100)
+
+# 41. write a program that take 2 numbers as input and check if both a and b are greater than 50  or a is greater than b
+#a = input()
+#b = input()
+print((a > 50 and b > 50)  or a > b)
+
+# 42. write a program that take number and check if given numbers are between 30 to 50
+#a = input()
+print(a > 30  and a < 50)
+
+# 43. write a program that take 2 inputs and check that any value is containing apple or not
+#a = input()
+#b = input()
+print(a == "apple" or b == "apple")
+
+# 44. write a program that take 3 inputs and check any value is less than 15 are not
+#a = input()
+#b = input()
+#c = input()
+print(a<  15 or b < 15 or c < 15)
+
+# 45. write a program that take 2 numbers as input and check if both value are positive or not
+#a = input()
+#b = input()
+print(a > 0 and b > 0)
+
+# 46. write a program that take age and status and if age should be between 12-60 and status should be yes
+#a = input()
+#b = input()
+print((a > 12 and a < 61) and b == "yes"  )
+
+# 47. write a program that take 2 value and any one value should be greater positive and product should be greater than 30
+#a = input()
+#b = input()
+print((a > 0 or b > 0 )and a * b > 30  )
+
+#https://learning.ccbp.in/course?c_id=a6454e48-d030-4d49-8920-253198052232&t_id=36b50c7e-806a-4834-859f-1c031bdf9942&s_id=a85f735e-5f33-4ba8-a198-27293964e4d1
+
